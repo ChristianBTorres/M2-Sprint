5c13965ed06632984c5c37224801faec667e44ce
Dann <daepgames@gmail.com>
2023-02-13 19:31:35 -0300

    Dann Espinoza: CRUD PACIENTE

---
+++ assets/JS/base.js
@@ -0,0 +1,25 @@
+const grid = new gridjs.Grid({
+  columns: [
+    "Nombre",
+    "Email",
+    "",
+   ],
+  data: Array(5).fill().map(x => [
+    faker.name.findName(),
+    faker.internet.email(),
+    gridjs.html(
+      "<a class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#staticBackdrop'>Editar</a>"
+    )
+  ])
+}).render(document.getElementById("tabla"));
+
+
+
+
+
+formatter: (cell, row) => {
+  return gridjs.h('button', {
+    className: 'btn btn-primary',
+    onClick: () => alert(`Editing "vvv"`)
+  }, 'Edit');
+}
